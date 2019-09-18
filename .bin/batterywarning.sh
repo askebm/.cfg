b372438598a1d918fef562dabfca1d762d41270d
Aske B.M <asmoe16@student.sdu.dk>
2019-09-18 16:32:27 +0200

    Added sripts

---
+++ .bin/batterywarning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while sleep 5; do
+if [ $( acpi | sed -E 's/.*:[^0-9]*([0-9]+).*/\1/g' ) -lt 10 ] ; then
+	dunstify "$(cowsay GIVE ME POWER!!!)"
+fi
+done
---
+++ .bin/dotfilerepostatus.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+shopt -s expand_aliases
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+TEXT=""
+if [ $(config status | grep "nothing to commit" | wc -l) -eq 0 ]
+then
+    #modified
+    TEXT="%{u#ff4b14}%{F-}"
+fi
+alias vimconf='/usr/bin/git --git-dir=$HOME/.vim/.git/ --work-tree=$HOME/.vim/'
+if [ $(vimconf status | grep "nothing to commit" | wc -l) -eq 0 ]
+then
+    #modified
+    TEXT=${TEXT}"%{u#ff4b14}%{F-}"
+fi
+
+#if [ $(config push -n 2>1 | grep "Everything up-to-date" | wc -l) -eq 1 ]
+#then
+#    # not modified
+#    TEXT="$TEXT"
+#else
+#    #modified
+#    TEXT="%{B#ffffff}"
+#fi
+echo -e $TEXT
